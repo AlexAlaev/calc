27004917bf08818131a0d90bd1792b970d3730cc
Michael Florya <mih2303@mail.ru>
2024-03-13 01:26:49 +0300

    variables are named by pep8, indented by pep8

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 # In the process of learning, I created a simple calculator in python
-x = float(input('Введите значение 1:'))
-y = float(input('Введите значение 2:'))
+first_num = float(input('Введите значение 1: '))
+second_num = float(input('Введите значение 2: '))
+
 # I ask the user to enter the numbers with which actions will be performed in the future
 print('Какое действие вы хотите сделать?')
-print('варианты ответа: +','сложение', '-', 'вычитание' '*','умножение', '/', 'деление')
-z = str(input())
-if z == '+':
-    print(x+y)
-    i = x+y
-elif z == '-':
-    print(x-y)
-    i = x-y
-elif z == '*':
-    print(x*y)
-    i = x*y
-elif z == '/':
-    print(x/y)
-    i = x/y
+print('варианты ответа: +', 'сложение', '-', 'вычитание' '*','умножение', '/', 'деление')
+
+operator = str(input("Введите оператор: "))
+
+if operator == '+':
+    print(first_num + second_num)
+    result = first_num + second_num
+elif operator == '-':
+    print(first_num - second_num)
+    result = first_num - second_num
+elif operator == '*':
+    print(first_num * second_num)
+    result = first_num * second_num
+elif operator == '/':
+    print(first_num / second_num)
+    result = first_num / second_num
 else:
     print('Вы выбрали непредусмотренное действие')
+
 # all actions of the calculator are performed through the function if
-print(i)
+# print(result)  Здесь лишний принт, т.к. в условных выражениях у тебя уже прописаны принты
 print('давайте продолжим, введите второе значение помимо полученного...')
-c = float(input('Введите значение:'))
+final_num = float(input('Введите значение: '))
 print('Какое действие вы хотите сделать?')
-k = str(input())
+
+operator2 = str(input("Введите оператор: "))
+
 # We ask the user to enter the following number to continue the action
-if k == '+':
-    print(i+c)
-if k == '-':
-    print(i-c)
-if k == '*':
-    print(i*c)
-if k == '/':
-    print(i/c)
+if operator2 == '+':
+    print(result + final_num)
+if operator2 == '-':
+    print(result - final_num)
+if operator2 == '*':
+    print(result * final_num)
+if operator2 == '/':
+    print(result / final_num)
+
 # all actions of the calculator are performed through the function if
