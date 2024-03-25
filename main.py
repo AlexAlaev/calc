997f237731e44e961199843d22479ebe4061655e
AlexAlaev <vdovinvdovinivan10@gmail.com>
2024-03-25 23:57:35 +0300

    I made the calculator infinite and combined it into a function

--- main.py
+++ main.py
@@ -1,83 +1,57 @@
-# In the process of learning, I created a simple calculator in python
-<<<<<<< HEAD
-first_num = float(input('Введите значение 1: '))
-second_num = float(input('Введите значение 2: '))
+def calc():
+    first_num = float(input('Введите значение 1: '))
+    second_num = float(input('Введите значение 2: '))
 
-# I ask the user to enter the numbers with which actions will be performed in the future
-print('Какое действие вы хотите сделать?')
-print('варианты ответа: +', 'сложение', '-', 'вычитание' '*','умножение', '/', 'деление')
+    # I ask the user to enter the numbers with which actions will be performed in the future
+    print('Какое действие вы хотите сделать?')
+    print('варианты ответа: +', 'сложение', '-', 'вычитание' '*', 'умножение', '/', 'деление')
 
-operator = str(input("Введите оператор: "))
+    operator = str(input("Введите оператор: "))
 
-if operator == '+':
-    print(first_num + second_num)
-    result = first_num + second_num
-elif operator == '-':
-    print(first_num - second_num)
-    result = first_num - second_num
-elif operator == '*':
-    print(first_num * second_num)
-    result = first_num * second_num
-elif operator == '/':
-    print(first_num / second_num)
-    result = first_num / second_num
-else:
-    print('Вы выбрали непредусмотренное действие')
+    if operator == '+':
+        print(first_num + second_num)
+        result = first_num + second_num
+    elif operator == '-':
+        print(first_num - second_num)
+        result = first_num - second_num
+    elif operator == '*':
+        print(first_num * second_num)
+        result = first_num * second_num
+    elif operator == '/':
+        print(first_num / second_num)
+        result = first_num / second_num
+    else:
+        print('Вы выбрали непредусмотренное действие')
 
-# all actions of the calculator are performed through the function if
-# print(result)  Здесь лишний принт, т.к. в условных выражениях у тебя уже прописаны принты
-print('давайте продолжим, введите второе значение помимо полученного...')
-final_num = float(input('Введите значение: '))
-print('Какое действие вы хотите сделать?')
+    # all actions of the calculator are performed through the function if
 
-operator2 = str(input("Введите оператор: "))
+    start = 'start'
+    while start != 'stop':
+        print('Если хотите завершить программу, то напишите "stop", если хотите продолжить, то введите что угодно')
+        stopper = str(input("Ввод: "))
+        if stopper == 'stop':
+            print('Программа завершена')
+            break
+        print('давайте продолжим, введите второе значение помимо полученного...')
+        final_num = float(input('Введите значение: '))
+        operator2 = str(input('Введите оператор: '))
 
-# We ask the user to enter the following number to continue the action
-if operator2 == '+':
-    print(result + final_num)
-if operator2 == '-':
-    print(result - final_num)
-if operator2 == '*':
-    print(result * final_num)
-if operator2 == '/':
-    print(result / final_num)
+        # We ask the user to enter the following number to continue the action
 
-# all actions of the calculator are performed through the function if
-=======
-x = float(input('Введите значение 1:'))
-y = float(input('Введите значение 2:'))
-# I ask the user to enter the numbers with which actions will be performed in the future
-print('Какое действие вы хотите сделать?')
-print('варианты ответа: +','сложение', '-', 'вычитание' '*','умножение', '/', 'деление')
-z = str(input())
-if z == '+':
-    print(x+y)
-    i = x+y
-elif z == '-':
-    print(x-y)
-    i = x-y
-elif z == '*':
-    print(x*y)
-    i = x*y
-elif z == '/':
-    print(x/y)
-    i = x/y
-else:
-    print('Вы выбрали непредусмотренное действие')
-# all actions of the calculator are performed through the function if
-print(i)
-print('давайте продолжим, введите второе значение помимо полученного...')
-c = float(input('Введите значение:'))
-print('Какое действие вы хотите сделать?')
-k = str(input())
-# We ask the user to enter the following number to continue the action
-if k == '+':
-    print(i+c)
-if k == '-':
-    print(i-c)
-if k == '*':
-    print(i*c)
-if k == '/':
-    print(i/c)
-# all actions of the calculator are performed through the function if
->>>>>>> Initial commit
+        if operator2 == '+':
+            print(result + final_num)
+            result += final_num
+        if operator2 == '-':
+            print(result - final_num)
+            result -= final_num
+        if operator2 == '*':
+            print(result * final_num)
+            result *= final_num
+        if operator2 == '/':
+            print(result / final_num)
+            result /= final_num
+
+        # all actions of the calculator are performed through the function if
+
+
+calc()
